b77fa9351851841e07ce3a1fc7eefaa5c42420da
jonathangreco <nataniel.greco@gmail.com>
2014-06-02 16:15:07 +0200

    update navigation

--- config/autoload/navigation.local.php
+++ config/autoload/navigation.local.php
@@ -4,12 +4,33 @@
  * @author Jonathan Greco <nataniel.greco@gmail.com>  
  */ 
 return array(
-	'navigation' => array(
-		'default' => array(
-			'Album' => array(
-				'label' => 'Album',
-				'route' => 'album',
-			),
+    'navigation' => array(
+        'default' => array(
+            'Album' => array(
+                'label' => 'Album',
+                'route' => 'album',
+                'pages' => array(
+                 array(
+                     'label' => 'Add',
+                     'route' => 'album/default',
+                     'action' => 'add',
+                 ),
+                 array(
+                     'label' => 'Edit',
+                     'route' => 'album/default',
+                     'action' => 'edit',
+                 ),
+                 array(
+                     'label' => 'Delete',
+                     'route' => 'album/default',
+                     'action' => 'delete',
+                 ),
+                 array(
+                     'label' => 'Page',
+                     'route' => 'album/page',
+                 ),
+                ),
+            ),
 		),
 	),
 );
