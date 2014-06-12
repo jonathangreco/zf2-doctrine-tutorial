d8957a5cf5ff51848c6249389b60f225426457ff
jonathangreco <nataniel.greco@gmail.com>
2014-06-12 11:27:27 +0200

    flash Message traduction

---
+++ module/Application/language/FlashMessage/En_US-flashMessage.php
@@ -0,0 +1,9 @@
+<?php
+/**
+ * Traduction des flashMessages en Anglais
+ */
+return array(
+	"Your album has been added." => "Your album has been added.",
+	"Your album has been edited." => "Your album has been edited.",
+	"Your album has been deleted." => "Your album has been deleted."
+);
